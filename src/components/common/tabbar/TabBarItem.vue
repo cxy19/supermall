49724f2a911835255d93a9f5f2f9154594f9b257
cxy19 <2313470604@qq.com>
2021-12-31 15:06:43 +0800

    1231commit

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tab-bar-item">
+    <div ><slot name='item-icon'></slot></div>
+    <div><slot name='item-text'></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBarItem'
+
+}
+</script>
+
+<style>
+
+</style>
